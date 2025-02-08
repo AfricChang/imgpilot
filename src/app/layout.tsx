71afcb3a3caab44061a23dcecb4615fa4a07381d
AfricChang <1334526911@qq.com>
2025-02-08 22:45:08 +0800

    delete google font downloading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { ReactNode } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ImgPilot ｜ Turn the draft into an amazing artwork",
@@ -21,7 +18,7 @@ const viewport: Viewport = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="font-['Inter']">{children}</body>
       <Analytics></Analytics>
     </html>
   );
